020d95b86938195403c93e5af34a993f46485348
Dax <isaiahcollins02@gmail.com>
2021-12-13 21:02:17 +0800

    experimental writes

---
+++ lib/characters/characters_notifier.dart
@@ -0,0 +1,23 @@
+import 'package:automator/characters/character.dart';
+import 'package:flutter/foundation.dart';
+
+class CharactersNotifier extends ChangeNotifier {
+  final List<Character> _characters = [];
+  List<Character> get characters => _characters;
+
+  put(Character character) {
+    if (_characters.contains(character)) {
+      final index = _characters.indexOf(character);
+      assert(index >= 0);
+      _characters[index] = character;
+    } else {
+      _characters.add(character);
+    }
+    notifyListeners();
+  }
+
+  remove(Character character) {
+    _characters.remove(character);
+    notifyListeners();
+  }
+}
