19f9ef1962f878cfb26ec285ce081d75e23b4c84
Dax <isaiahcollins02@gmail.com>
2021-12-29 14:36:25 +0800

    character editor preview

---
+++ lib/shared/custom/checkbox_form.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class CheckboxForm extends StatelessWidget {
+  const CheckboxForm({
+    Key? key,
+    required this.value,
+    required this.onChanged,
+    required this.title,
+    this.enabled = true,
+  }) : super(key: key);
+  final bool value;
+  final Function(bool?) onChanged;
+  final Widget title;
+  final bool enabled;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: enabled
+          ? () {
+              onChanged(!value);
+            }
+          : null,
+      child: Row(
+        children: [
+          Checkbox(
+            value: value,
+            onChanged: enabled ? onChanged : null,
+          ),
+          Expanded(
+            child: title,
+          )
+        ],
+      ),
+    );
+  }
+}
